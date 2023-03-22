69ec5178d5dae01f9c9c6a598bd0c323200c88f7
SevenAJJY <yassine.elhajjy@gmail.com>
2023-03-22 23:46:13 +0000

    added functions to get and show countries informations

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const searchWrapper = document.querySelector(".search-input");
 const inputBox = document.querySelector(".search-input input");
 const suggBox = document.querySelector(".autocom-box");
 const searchBtn = document.querySelector('.icon');
+const resultBox = document.querySelector('.result');
+const listInfo = resultBox.querySelector('.list-info');
 
 let suggestions = [];
 
@@ -26,7 +28,68 @@ async function getAllCountriesNames() {
 getAllCountriesNames();
 
 
+// if user click on search button
+searchBtn.addEventListener('click', getInfo);
 
+async function getInfo() {
+    let countryName = inputBox.value;
+    try {
+        let response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
+        let countryInfo = await response.json();
+        showInfo(countryInfo);
+    } catch (er) {
+        console.log(er);
+    }
+}
+
+function showInfo(country) {
+    let info = `
+        <img src="${country[0].flags.svg}" class="flag-img" />
+        <h2>${country[0].name.common}</h2>
+        <ul class="list-info">
+            <li>
+                <i class="fa-solid fa-flag"></i>
+                <span>Native Name:</span>
+                <span>${country[0].name.common}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-landmark-flag"></i>
+                <span>Capital:</span>
+                <span>${country[0].capital[0]}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-people-group"></i>
+                <span>Population:</span>
+                <span>${country[0].population}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-globe"></i>
+                <span>Region:</span>
+                <span>${country[0].region}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-earth-africa"></i>
+                <span>Sub-region:</span>
+                <span>${country[0].subregion}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-language"></i>
+                <span>Languages:</span>
+                <span>${Object.values(country[0].languages)
+                    .toString()
+                    .split(',')
+                    .join(', ')}</span>
+            </li>
+            <li>
+                <i class="fa-solid fa-coins"></i>
+                <span>Currencies:</span>
+                <span>${country[0].currencies[Object.keys(country[0].currencies)].name} - ${Object.keys(country[0].currencies)[0]}</span>
+            </li>
+    </ul>
+    `;
+
+    resultBox.innerHTML = info;
+}
 
 // if user press any key and release
 inputBox.addEventListener('keyup', (e) => {
