19ea9c8fb0f9878d6cfc6b25fd3fa9871ed070fe
SevenAJJY <yassine.elhajjy@gmail.com>
2023-03-22 19:10:00 +0000

    Added function to get all country names and put them into a suggestion matrix for autocomplete

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-let suggestions = [
-    "Morocco",
-    "India",
-    "spain",
-    "Germany",
-    "Argentine",
-    "Australia",
-    "France",
-    "Italie"
-];
-
 // getting All required Elements
 
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = document.querySelector(".search-input input");
 const suggBox = document.querySelector(".autocom-box");
+
+let suggestions = [];
+
+
+// get all countries names and store him in  suggestions
+
+async function getAllCountriesNames() {
+    try {
+        let response = await fetch('https://restcountries.com/v3.1/all');
+        countries = await response.json();
+        countries.forEach(country => {
+            suggestions.push(country.name.common);
+        });
+        suggestions = suggestions.sort();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+console.log(suggestions);
+
+getAllCountriesNames();
 
 // if user press any key and release
 inputBox.addEventListener('keyup', (e) => {
@@ -24,7 +35,11 @@ inputBox.addEventListener('keyup', (e) => {
         emptyArray = suggestions.filter((data) => {
             return data.toLowerCase().startsWith(userData.toLowerCase());
         });
+        console.log(emptyArray);
         emptyArray = emptyArray.map((data) => {
+            data.split("").forEach((span) => {
+                console.log(span);
+            })
             return data = `<li>${data}</li>`;
         });
         searchWrapper.classList.add('active');
