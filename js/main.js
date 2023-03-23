acb2663c2148d79cf4e4d10b74935b36d5d6d59d
SevenAJJY <yassine.elhajjy@gmail.com>
2023-03-23 05:14:52 +0000

    CLEANING -_-

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ async function getAllCountriesNames() {
         countries.forEach(country => {
             suggestions.push(country.name.common);
         });
+        // sorting countries a-z
         suggestions = suggestions.sort();
     } catch (error) {
         console.log(error);
@@ -36,16 +37,20 @@ getAllCountriesNames();
 searchBtn.addEventListener('click', getInfo);
 
 function getInfo() {
+
     resultBox.innerHTML = "";
     searchWrapper.classList.remove('active');
     //show error if the user doesn't entered country name
     if (inputBox.value.length != 0) {
+
         loader.classList.toggle('active');
         setTimeout(async() => {
             // validate country name entered by the user
             if (validateCountryName()) {
+
                 loader.classList.toggle('active');
                 let countryName = inputBox.value;
+
                 try {
                     let response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
                     let countryInfo = await response.json();
@@ -75,6 +80,7 @@ function validateCountryName() {
 
 
 function showInfo(country) {
+
     let info = `
         <header>
             <p>Informations</p>
@@ -119,7 +125,8 @@ function showInfo(country) {
             <li>
                 <i class="fa-solid fa-language"></i>
                 <span>Languages:</span>
-                <span>${Object.values(country[0].languages)
+                <span>${
+                    Object.values(country[0].languages)
                     .toString()
                     .split(',')
                     .join(', ')}</span>
@@ -128,7 +135,9 @@ function showInfo(country) {
             <li>
                 <i class="fa-solid fa-coins"></i>
                 <span>Currencies:</span>
-                <span>${country[0].currencies[Object.keys(country[0].currencies)].name} - ${Object.keys(country[0].currencies)[0]}</span>
+                <span>
+                ${country[0].currencies[Object.keys(country[0].currencies)].name} - 
+                ${Object.keys(country[0].currencies)[0]}</span>
             </li>
     </ul>
     `;
@@ -169,10 +178,15 @@ inputBox.addEventListener('keyup', (e) => {
         let allList = suggBox.querySelectorAll('li');
         allList.forEach((li) => {
             li.addEventListener('click', (e) => {
+
                 selectUserData = e.target.textContent;
+
                 inputBox.value = selectUserData;
+
                 searchWrapper.classList.remove('active');
+
                 searchBtn.click();
+
             })
         })
     } else {
@@ -181,9 +195,11 @@ inputBox.addEventListener('keyup', (e) => {
 
 });
 
-
+// show Suggestions (autocomplete)
 function showSuggestions(list) {
+
     let listData;
+
     if (!list.length) {
         userValue = inputBox.value;
         listData = `<li>${userValue}</li>`;
@@ -207,14 +223,17 @@ const themeLightDark = (e) => {
 
 
     if (e.currentTarget.classList.contains('fa-sun')) {
+
         e.currentTarget.classList.replace('fa-sun', 'fa-moon');
         localStorage.setItem('t_dark', true);
         themeDark();
 
     } else {
+
         e.currentTarget.classList.replace('fa-moon', 'fa-sun');
         localStorage.setItem('t_dark', false);
         themeDark();
+
     }
 
 
@@ -225,8 +244,10 @@ const themeLightDark = (e) => {
 
 
 if (localStorage.getItem('t_dark') == 'true') {
+
     darkLightMode.classList.replace('fa-sun', 'fa-moon');
     document.body.classList.add('t-dark');
+
 }
 
 
